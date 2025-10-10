439c7461fa6d4e3a4eb4608743c0d99387309ede
SUNXU <oca2402ai0001@edu.oca.ac.jp>
2025-10-10 13:54:32 +0900

    Delete search_practice directory

--- search_practice/config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  root "posts#index"
-
-  devise_for :users
-
-  resources :posts, only: [:index, :show, :new, :create]
-  resources :mypages, only: [:index, :show]  # 用户搜索/用户详情
-end
